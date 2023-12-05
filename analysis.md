20b82d539395f50558ca81a404bdf94da455b344
khawla-Moh <khawlaMoh00@gmail.com>
2023-12-05 22:39:32 +0300

    add models

---
+++ analysis.md
@@ -0,0 +1,68 @@
+Amazon E-commerc:
+
+# Product:
+
+**data:**
+
+- name
+- flag(new,sale,Feature)
+- price
+- image
+- images
+- reviews
+- reviews count
+- brand :
+  -image
+  -name
+  -item count
+
+- sku
+- subtitle
+- tags
+- related
+- description
+
+- **Function:**
+
+- list
+- detail
+- brand list
+- brand detail
+- search
+- filter
+- add to cart
+- add to whishlist
+-
+
+## USER:
+
+**data:**
+
+- username
+
+- email
+
+- image
+
+- contact number:
+
+  type(primary-seconadary)
+
+  number
+
+- adress:
+
+  type(home,offiece,acdemy,bussiness)
+
+  adress
+
+## function:
+
+- auth
+- dashboard
+- profile
+- edit profile
+
+models:
+
+product,user,review,brand
--- products/models.py
+++ products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+FLAG_TYPE=(
+
+    ('New','New'),
+    ('Sale','Sale'),
+    ('Feature','Feature')
+)
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length= 150)
+    flag=models.CharField(max_length=15,choices=FLAG_TYPE)
+    price=models.FloatField()
+    image=models.ImageField(upload_to='product')
+    sku=models.IntegerField()
+    subtitle=models.TextField(max_length=400)
+    description=models.TextField(max_length=20000)
+    tags = TaggableManager() 
+    brand=models.ForeignKey('Brand',related_name='product_prand',on_delete=models.SET_NULL,null=True)
+
+class ProductImages(models.Model):
+    Product=models.ForeignKey(Product,related_name='product_images',on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='productimages')
+
+
+
+class Reviews(models.Model):
+    user=models.ForeignKey(User,related_name='reviews_user',on_delete=models.SET_NULL,null=True)
+    product=models.ForeignKey(Product,related_name='reviews_product',on_delete=models.CASCADE)
+    review=models.TextField(max_length=700)
+    date=models.DateTimeField(default=timezone.now)
+    rate=models.IntegerField(choices=[(i,i) for i  in range(1,6)])
+
+
+class Brand(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='brand')
+
+
+    
+class User(models.Model):
+    pass
+class UserPhone(models.Model):
+    pass
+class UserAdrees(models.Model):
+    pass
