aa5761c27065bc16515dcbf4d8a7896b8f63aee6
khawla-Moh <khawlaMoh00@gmail.com>
2023-12-09 17:27:16 +0300

    add productlist && productdetail

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ProductList,ProductDetail
+
+
+
+
+urlpatterns = [
+     path('',ProductList.as_view()),
+     path('<slug:slug>',ProductDetail.as_view())
+]
