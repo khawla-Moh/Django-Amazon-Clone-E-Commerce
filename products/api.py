c87dfa54225a7d289f1892e765a37b5d7de969e1
khawla-Moh <khawlaMoh00@gmail.com>
2023-12-19 20:29:03 +0300

    search api data

--- products/api.py
+++ products/api.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework import filters
 
 from .models import Product,Brand,Reviews,ProductImages
 from . import serializers
@@ -13,8 +13,10 @@ from .pagination import ApiPagination
 class ProductListAPI(generics.ListAPIView):
     queryset=Product.objects.all()
     serializer_class=serializers.ProductListSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
     filterset_fields = ['brand', 'flag']
+    search_fields = ['name', 'description','subtitle']
+    ordering_fields = ['name']
 
 
 class ProductDetailAPI(generics.ListAPIView):
@@ -27,7 +29,10 @@ class BrandListAPI(generics.ListAPIView):
     queryset=Brand.objects.all()
     serializer_class=serializers.BrandListSerializer    
     pagination_class=ApiPagination
-   
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['name', ]
+    ordering_fields = ['name']
+ 
 
 class BrandDetailAPI(generics.ListAPIView):
     queryset=Brand.objects.all()
