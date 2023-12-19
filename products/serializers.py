7bb76b21b4a476f531ce5eea747061e6dec32354
khawla-Moh <khawlaMoh00@gmail.com>
2023-12-19 21:50:44 +0300

    productimage reviews product api

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,21 @@
 from rest_framework import serializers
-from .models import Product,Brand
+from .models import Product,Brand,Reviews,ProductImages
+
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model=ProductImages
+        fields=['image']
+
+
+
+class ProductReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model=Reviews
+        fields=['user','rate','date','review']
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -34,7 +50,8 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     brand=serializers.StringRelatedField()
     review_count=serializers.SerializerMethodField(method_name='get_review_count')
     avg_rate=serializers.SerializerMethodField()
-   
+    images=ProductImageSerializer(source='product_images',many=True)
+    reviews=ProductReviewSerializer(source='reviews_product',many=True)
     class Meta:
         model=Product
         fields='__all__'
