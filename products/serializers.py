5b894fd5bc1bdc4a931d603635bd548792e5c22b
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-05 23:09:24 +0300

    showed review_count & avg_rate with fields in productdetiales serializers

--- products/serializers.py
+++ products/serializers.py
@@ -53,26 +53,10 @@ class ProductListSerializer(serializers.ModelSerializer,TaggitSerializer,):
 class ProductDetailSerializer(serializers.ModelSerializer,TaggitSerializer):
     brand=serializers.StringRelatedField()
     tags = TagListSerializerField()
-   
-    """ 
-    review_count=serializers.SerializerMethodField(method_name='get_review_count')
-    avg_rate=serializers.SerializerMethodField()
-    """
-    """
-    #to make avg_rate and count_review showed in product detail api
-    review_count=serializers.IntegerField()
-    avg_rate=serializers.FloatField()
-     """
     images=ProductImageSerializer(source='product_images',many=True)
     reviews=ProductReviewSerializer(source='reviews_product',many=True)
-    class Meta:
-        model=Product
-        fields=['__all__']
-
-
-
-
-""" 
+   
+    """ 
     def get_review_count(self,object):
         reviews=object.review_count()
         return reviews
@@ -81,8 +65,43 @@ class ProductDetailSerializer(serializers.ModelSerializer,TaggitSerializer):
         avg=object.avg_rate()
                  
         return avg    
- """
+    """
 
+    """ 
+    *****************************************************************************
+    review_count=serializers.SerializerMethodField(method_name='get_review_count')
+    avg_rate=serializers.SerializerMethodField()
+    *****************************************************************************
+    """
+    """
+    *****************************************************************************
+    #to make avg_rate and count_review showed in product detail api
+    review_count=serializers.IntegerField()
+    avg_rate=serializers.FloatField()
+    *****************************************************************************
+    """
+    
+    """ 
+    ********************************************************************************
+        def get_review_count(self,object):
+            reviews=object.review_count()
+            return reviews
+    
+        def get_avg_rate(self,object):
+            avg=object.avg_rate()
+                    
+            return avg    
+    **********************************************************************************        
+    """
+    class Meta:
+   
+        model=Product
+        fields=['name','flag','price','sku','subtitle','description','brand','review_count','avg_rate','tags','images','reviews']
+
+
+
+
+  
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
         model=Brand
