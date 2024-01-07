2bb3978287fcef520742e2fd6f0455d5fabb4a4d
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-08 01:14:01 +0300

    order_detail address

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+ADDRESS_TYPE=(
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Bussines','Bussines'),
+    ('Other','Other')
+)
+
+class Address(models.Model):
+    user=models.ForeignKey(User,related_name='address_user',on_delet=models.CASCADE)
+    address=models.TextField(max_length=200)
--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
+from utils.generate_code import generate_code
+from django.utils import timezone
+from accounts.models import Address
+
 
 ORDER_STATE=(
 ('Reecieved','Reecieved'),
@@ -12,14 +16,22 @@ ORDER_STATE=(
 
 # Create your models here.
 
-class Orders(models.Model):
+class Order(models.Model):
     user=models.ForeignKey(User,related_name='orders_woner',on_delet=models.SET_NULL,null=True)
     state=models.CharField(choices=ORDER_STATE,max_length=12)
-    product=models.ForeignKey(Product,related_name='orders_product',on_delete=models.SET_NULL,null=True)
+    code=models.CharField(defualt=generate_code)
+    order_time=models(defualt=timezone.now)
+    delivery_time=models(null=True,blank=True)
+    delivery_address=models.ForeignKey(Address,related_name='delivery_address',on_delete=models.SET_NULL,null=True,blank=True)
 
 
 
 
 
 class OrderDetail(models.Model):
-    pass
+    
+    order=models.ForeignKey(Order,related_name='order_detail',on_delate=models.CASCADE)
+    product=models.ForeignKey(Product,related_name='orderdetail_product',on_delete=models.SET_NULL,null=True)
+    quantity=models.IntegerField()
+    price=models.FloatField()
+    total=models.FloatField()
