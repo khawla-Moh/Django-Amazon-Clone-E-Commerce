94285ee59e947e21b08b463fd11ade255f2c9c42
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-13 15:43:17 +0300

    queryset lazy queryset cache

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Order
 # Create your views here.
+def ordersData(request):
+    data=Order.object.all().ordey_by('price')
+    data2=Order.object.all().filter()
