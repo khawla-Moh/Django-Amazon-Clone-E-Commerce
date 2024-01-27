fe650b4575d0d1d8b9a30ed60d8ac6b2b4437b98
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-27 21:54:12 +0300

    profile model design

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Address
+from .models import Address,Profile,ContactNumbers
 # Register your models here.
 admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(ContactNumbers)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup(request):
+    pass
+    '''
+      -create new user
+      -send email (code activation):code
+      -redirct to acitvate page
+    '''
+def user_activate(request):
+    pass
+    '''
+    -recivee code activation
+    -redirect to login page
+    '''    
