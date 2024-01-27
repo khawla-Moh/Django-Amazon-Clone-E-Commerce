02b0162b9342b6a3805389c0363625ae078aa548
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-27 20:04:29 +0300

    ContactNumbers model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 # Create your models here.
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='profile')
+    code=models.CharField(max_length=10,defualt=generate_code)
+
+    def __str__(self):
+        return str(self.user)
+
+
+
+PHONE_TYPE=(
+    ('Primary','Primary'),
+    ('Secondary','Secondary'),
+    
+)
+
+class ContactNumbers(models.Model):
+    user=models.ForeignKey(User,related_name='user_adddress',on_delete=models.CASCADE)
+    type=models.CharField(max_length=10,choices=PHONE_TYPE)
+    number=models.CharField(max_length=25)
 
 
 
