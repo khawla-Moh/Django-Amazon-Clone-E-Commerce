0488b486484c079f45634613c46e92d8309fc4d1
khawla-Moh <khawlaMoh00@gmail.com>
2024-01-28 23:49:58 +0300

    signup function

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+
+from .forms import SignupForm,UserActivateForm
 
 # Create your views here.
 
 def signup(request):
-    pass
-    '''
+   if request.method=='POST':
+    form=SignupForm(request.POST)
+    if form.is_valid():
+       form.save()                      #triggle signal ---> create profile ---> code 
+
+   else:
+    form=SignupForm
+   
+   return render(request,'account/signup.html',{'form':form})
+
+   '''
       -create new user
       -send email (code activation):code
       -redirct to acitvate page
     '''
 def user_activate(request):
-    pass
+    if request.method=='POST':
+        form=UserActivateForm(request.POST)
+
+    else:
+        form=SignupForm
+    
+    return render(request,'account/activate.html',{'form':form})
+
     '''
     -recivee code activation
     -redirect to login page
