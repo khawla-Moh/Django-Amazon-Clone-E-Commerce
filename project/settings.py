79bf7a1207be0b3291a3c5d03b9bdcdc6c435184
khawla-Moh <khawlaMoh00@gmail.com>
2024-02-10 16:50:26 +0300

    .

--- project/settings.py
+++ project/settings.py
@@ -127,24 +127,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 """ 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
  """
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "amazon",
+        "NAME": "Amazon",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "db",
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
